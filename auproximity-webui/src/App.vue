093337b65a7104b1026c4dc982cf2808b05140a6
Bobbyjeff9876 <70022876+Bobbyjeff9876@users.noreply.github.com>
2020-12-21 10:39:16 -0500

    Update App.vue

--- auproximity-webui/src/App.vue
+++ auproximity-webui/src/App.vue
@@ -11,7 +11,7 @@
           width="80"
         />
 
-        <h2 class="px-3 hidden-xs-only">Among Us Proximity Voice</h2>
+        <h2 class="px-3 hidden-xs-only">Among Us Indoor Chat V2</h2>
       </div>
 
       <v-spacer></v-spacer>
